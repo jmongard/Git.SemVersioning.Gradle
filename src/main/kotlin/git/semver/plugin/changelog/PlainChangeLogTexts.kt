1b3936cf05b4ab43b8071e38b6d67c05a5f658a4
Tim Tran <49887921+tacascer@users.noreply.github.com>
2023-11-26 00:40:51 -0500

    feat: Add plain change log texts

---
+++ src/main/kotlin/git/semver/plugin/changelog/PlainChangeLogTexts.kt
@@ -0,0 +1,35 @@
+package git.semver.plugin.changelog
+
+
+internal object PlainHeaderTexts {
+    const val TOP_HEADER = "## What's Changed"
+    const val BREAKING_CHANGES = "### Breaking Changes"
+    const val OTHER_CHANGES = "### Other Changes"
+    const val BUG_FIXES = "### Bug Fixes"
+    const val NEW_FEATURE = "### New Features"
+    const val TESTS = "### Tests"
+    const val DOCUMENTATION = "### Documentation"
+    const val DEPENDENCY_UPDATES = "### Dependency Updates"
+    const val BUILD = "### Build & CI"
+    const val CI = BUILD
+    const val CHORES = "### Chores"
+    const val PERFORMANCE_ENHANCEMENTS = "### Performance Enhancements"
+    const val REFACTORINGS = "### Refactorings"
+}
+object PlainChangeLogTexts : ChangeLogTexts {
+    override val headerTexts = mutableMapOf(
+        ChangeLogTexts.HEADER to PlainHeaderTexts.TOP_HEADER,
+        ChangeLogTexts.BREAKING_CHANGE to PlainHeaderTexts.BREAKING_CHANGES,
+        ChangeLogTexts.OTHER_CHANGE to PlainHeaderTexts.OTHER_CHANGES,
+        "fix" to PlainHeaderTexts.BUG_FIXES,
+        "feat" to PlainHeaderTexts.NEW_FEATURE,
+        "test" to PlainHeaderTexts.TESTS,
+        "docs" to PlainHeaderTexts.DOCUMENTATION,
+        "deps" to PlainHeaderTexts.DEPENDENCY_UPDATES,
+        "build" to PlainHeaderTexts.BUILD,
+        "ci" to PlainHeaderTexts.CI,
+        "chore" to PlainHeaderTexts.CHORES,
+        "perf" to PlainHeaderTexts.PERFORMANCE_ENHANCEMENTS,
+        "refactor" to PlainHeaderTexts.REFACTORINGS
+    )
+}
